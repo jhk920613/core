76b23e96ba45c9f532c322336c34b283da9b3b7d
jhk920613 <926jhk@gmail.com>
2021-04-27 22:42:00 +0900

    0427  - 스프링 핵심 원리 이해2 - 객체 지향 원리 적용    . 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구성영역
+@Configuration
 public class AppConfig {
 
     // 역할
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 역할
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 역할
+    @Bean
     public OrderService orderService() {
         // 구현
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할
+    @Bean
     public DiscountPolicy discountPolicy() {
         // 구현
 //        return new FixDiscountPolicy();
