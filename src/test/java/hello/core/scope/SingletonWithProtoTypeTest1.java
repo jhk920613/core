14c1fc7c38563d022808eee9f76c44aeac0b108a
jhk920613 <926jhk@gmail.com>
2021-05-06 22:42:19 +0900

    0506  - 빈 스코프    . 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제해결

--- src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -28,7 +30,7 @@ public class SingletonWithProtoTypeTest1 {
     }
 
     @Test
-    void singletonClientUseProtytype() {
+    void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
@@ -36,34 +38,71 @@ public class SingletonWithProtoTypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
+    // 자바 표준 사용
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입, 주입 이후 계속 같은걸 사
+        private final Provider<PrototypeBean> prototypeBeanProvider;  // 생성 시점에 주입, 주입 이후 계속 같은걸 사
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
-            prototypeBean.addCount();
+            PrototypeBean object = prototypeBeanProvider.get();
+            object.addCount();
 
-            return prototypeBean.getCount();
+            return object.getCount();
         }
-        // logic 을 호출할 때 마다 prototypeBean 이 새로 생성되길 원하면 무식하지만 아래 방법을 사용해야 함
-        // 더 좋은 해결 방법이 있으므로 이 방식은 사용하지 말 것
+    }
+
+    // 변경
+//    @Scope("singleton")
+//    static class ClientBean {
+//        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;  // 생성 시점에 주입, 주입 이후 계속 같은걸 사
+//
 //        @Autowired
-//        ApplicationContext applicationContext;
+//        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanProvider = prototypeBeanProvider;
+//        }
 //
 //        public int logic() {
-//            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+//            PrototypeBean object = prototypeBeanProvider.getObject();
+//            object.addCount();
+//
+//            return object.getCount();
+//        }
+//    }
+
+    // 기존
+//    @Scope("singleton")
+//    static class ClientBean {
+//        private final PrototypeBean prototypeBean;  // 생성 시점에 주입, 주입 이후 계속 같은걸 사
+//
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic() {
 //            prototypeBean.addCount();
+//
 //            return prototypeBean.getCount();
 //        }
-    }
+//        // logic 을 호출할 때 마다 prototypeBean 이 새로 생성되길 원하면 무식하지만 아래 방법을 사용해야 함
+//        // 더 좋은 해결 방법이 있으므로 이 방식은 사용하지 말 것
+////        @Autowired
+////        ApplicationContext applicationContext;
+////
+////        public int logic() {
+////            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+////            prototypeBean.addCount();
+////            return prototypeBean.getCount();
+////        }
+//    }
 
     // 이 것도 빈 등록하면 주입받는 prototypeBean은 위 prototypeBean과는 다른 인스턴스
     @Scope("singleton")
